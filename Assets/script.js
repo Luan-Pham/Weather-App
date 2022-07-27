dfe05996c13f3f084fa6d40525f845ba1415f878
unknown <phamexb@gmail.com>
2022-07-26 21:26:09 -0700

    added function to display fetched data

--- Assets/script.js
+++ Assets/script.js
@@ -5,13 +5,27 @@
 var searchBtn = document.getElementById("searchBtn");
 
 function search() {
+  var location = document.getElementById("location").value;
+  console.log(location);
   var apiURL =
-    "api.openweathermap.org/data/2.5/forecast?q=" +
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
     location +
-    "&appid=84f4552f8265e604690655d0c9488cfe";
+    "&units=imperial&appid=84f4552f8265e604690655d0c9488cfe";
   fetch(apiURL)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => (console.log(data), displayWeather(data)));
+}
+
+function displayWeather(data) {
+  const { temp, humidity } = data.list[0].main;
+  const { main, description, icon } = data.list[0].weather[0];
+
+  console.log(temp, humidity);
+  console.log(main, description, icon);
+  // const { name } = data;
+  // const { description } = data.list[0].weather;
+  // console.log(description);
+  // console.log(name);
 }
 
 searchBtn.addEventListener("click", search);
