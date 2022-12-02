ca3aa33e6b632b3a643b1944dbabbb73b9c1048d
unknown <phamexb@gmail.com>
2022-12-02 15:53:12 -0800

    restarted index.html

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,6 @@
 // API key = 84f4552f8265e604690655d0c9488cfe
 
-// 5 day forecast API = api.openweathermap.org/data/2.5/forecast?q={city name},{country code}&appid={API key}
+// 5 day forecast API = api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
 
 var searchBtn = document.getElementById("searchBtn");
 var Day0 = document.getElementById("0");
