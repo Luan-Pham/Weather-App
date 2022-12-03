54f8e9070a6283ae96683b599fc55d54460cb17b
unknown <phamexb@gmail.com>
2022-12-02 19:21:16 -0800

    fixed css linking

--- Assets/script.js
+++ Assets/script.js
@@ -49,27 +49,15 @@ function displayForecast(data) {
   }
 }
 
-function oldSearch() {
-  $(btnContainer).append(`<button
-  class="btn btn-primary"
-  type="submit"
-  value="submit"
-  id="button-addon2"
->
-  Search
-</button>`);
-}
-
-`<div class="card" style="width: 18rem">
-<div class="card-body">
-  <h5 class="card-title">Card title</h5>
-  <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-  <p class="card-text">
-    Some quick example text to build on the card title and make
-    up the bulk of the card's content.
-  </p>
-  <a href="#" class="card-link">Card link</a>
-  <a href="#" class="card-link">Another link</a>
-</div>`;
+// function oldSearch() {
+//   $(btnContainer).append(`<button
+//   class="btn btn-primary"
+//   type="submit"
+//   value="submit"
+//   id="button-addon2"
+// >
+//   Search
+// </button>`);
+// }
 
 searchBtn.addEventListener('click', search);
